6b401af2f163df1fc19b32a3151f94a6cd6cb5df
bruno0409 <brunooliveiradesign@hotmail.com>
2025-06-18 21:13:41 -0300

    convite pronto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Dec 20, 2025 19:00:00");
+const dataDoEvento = new Date("jun 29, 2025 20:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function () {
